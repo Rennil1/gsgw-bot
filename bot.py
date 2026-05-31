ee4e935acfea7357daa691353ca94385407691dc
Rennil1 <nilumuhammadi@gmail.com>
2026-06-01 00:14:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+print("BOT_TOKEN =", os.getenv("BOT_TOKEN"))
+
 app = Client(
     "gsgw_bot",
     bot_token=os.getenv("BOT_TOKEN")
@@ -12,19 +14,7 @@ STORAGE_LINK = "https://t.me/+T2eOSVsgXANiMTU6"
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply(
-        "Welcome to Got Dropped into a Ghost Story, Still Gotta Work!",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("📚 Read Manhwa", url=CHANNEL_LINK)],
-                [InlineKeyboardButton("📦 Chapter Storage", url=STORAGE_LINK)]
-            ]
-        )
-    )
+    await message.reply("Bot works!")
 
 if __name__ == "__main__":
-    try:
-        app.run()
-    except Exception as e:
-        print(f"ERROR: {e}")
-        raise
+    app.run()
