1dcdba370199b5c8015a43fbafabecd2a55f59c8
Rennil1 <nilumuhammadi@gmail.com>
2026-06-01 01:24:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
 import os
 from pyrogram import Client, filters
 
-app = Client(
-    "gsgw_bot",
-    api_id=int(os.getenv("API_ID")),
-    api_hash=os.getenv("API_HASH"),
-    bot_token=os.getenv("BOT_TOKEN")
-)
+try:
+    app = Client(
+        "gsgw_bot",
+        api_id=int(os.getenv("API_ID")),
+        api_hash=os.getenv("API_HASH"),
+        bot_token=os.getenv("BOT_TOKEN")
+    )
 
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply("Bot works!")
+    @app.on_message(filters.command("start"))
+    async def start(client, message):
+        await message.reply("Bot works!")
 
-if __name__ == "__main__":
+    print("STARTING BOT...")
     app.run()
+
+except Exception as e:
+    print("ERROR:", repr(e))
+    raise
