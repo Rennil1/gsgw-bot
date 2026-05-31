d8b60c14d9bcdb5b38486605e7736c4def21d92f
Rennil1 <nilumuhammadi@gmail.com>
2026-06-01 01:09:05 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,7 @@
 import os
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-print("BOT_TOKEN =", os.getenv("BOT_TOKEN"))
+print("BOT_TOKEN =", bool(os.getenv("BOT_TOKEN")))
+print("API_ID =", os.getenv("API_ID"))
+print("API_HASH =", bool(os.getenv("API_HASH")))
 
-app = Client(
-    "gsgw_bot",
-    api_id=int(os.getenv("API_ID")),
-    api_hash=os.getenv("API_HASH"),
-    bot_token=os.getenv("BOT_TOKEN")
-)
-
-CHANNEL_LINK = "https://t.me/GSGW01"
-STORAGE_LINK = "https://t.me/+T2eOSVsgXANiMTU6"
-
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply("Bot works!")
-
-if __name__ == "__main__":
-    app.run()
+print("TEST SUCCESS")
