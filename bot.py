d99d410f8846348e5921d382bc4663de993af8f8
Rennil1 <nilumuhammadi@gmail.com>
2026-06-01 01:41:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,3 @@
-import os
-from pyrogram import Client
-
-try:
-    app = Client(
-        "gsgw_bot",
-        api_id=int(os.getenv("API_ID")),
-        api_hash=os.getenv("API_HASH"),
-        bot_token=os.getenv("BOT_TOKEN")
-    )
-
-    print("Starting bot...")
-    app.run()
-
-except Exception as e:
-    print("ERROR:", repr(e))
-    raise
+print("HELLO 1")
+print("HELLO 2")
+print("HELLO 3")
