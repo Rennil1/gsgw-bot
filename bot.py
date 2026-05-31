9bb8917a3c226eb913e74a9b7b1b707f1ececadd
Rennil1 <nilumuhammadi@gmail.com>
2026-05-31 22:38:39 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+app = Client(
+    "gsgw_bot",
+    bot_token="BOT_TOKEN"
+)
+
+CHANNEL_LINK = "https://t.me/GSGW01"
+STORAGE_LINK = "https://t.me/+T2eOSVsgXANiMTU6"
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply(
+        "Welcome to Got Dropped into a Ghost Story, Still Gotta Work!",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("📚 Read Manhwa", url=CHANNEL_LINK)],
+                [InlineKeyboardButton("📦 Chapter Storage", url=STORAGE_LINK)]
+            ]
+        )
+    )
+
+app.run()
